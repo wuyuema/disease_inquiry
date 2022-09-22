c6b87e1b30523445f1508673fed3267878ca5599
wuyuema <wuyuema20020528@163.com>
2022-09-22 22:49:39 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
-import a
+import mainUI
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWindow = QMainWindow()
-    ui = a.Ui_MainWindow()
+    ui = mainUI.Ui_MainWindow()
     ui.setupUi(mainWindow)
     mainWindow.show()
     sys.exit(app.exec_())
